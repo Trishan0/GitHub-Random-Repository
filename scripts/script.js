0f62c41f2a3f3e27932cf6223bed3e36ec62273f
Trishan <sanjanatrishan@gmail.com>
2025-04-08 16:10:00 +0530

    Add function to get random repository fetch by language

--- scripts/script.js
+++ scripts/script.js
@@ -23,3 +23,53 @@ async function getLanguages() {
 }
 getLanguages()
 
+let select = document.getElementById('lang')
+let selectedLang;
+select.addEventListener('change', (e) => {
+    selectedLang = e.target.value
+    console.log(selectedLang)
+})
+
+async function getRandomRepo(language) {
+    const url = `https://api.github.com/search/repositories?q=language:${language}&sort=stars&order=desc`;
+
+    try {
+        const response = await fetch(url);
+
+        // Check if rate limit is exceeded
+        if (response.status === 403) {
+            const errorJson = await response.json();
+            if (errorJson.message && errorJson.message.includes("API rate limit exceeded")) {
+                console.error("API rate limit exceeded. Please wait or authenticate with a personal access token.");
+            }
+        }
+
+        // Check for other response errors
+        if (!response.ok) {
+            console.error(`Response status: ${response.status}`);
+        }
+
+        const json = await response.json();
+
+        if (json.items && json.items.length > 0) {
+            const randomIndex = Math.floor(Math.random() * json.items.length);
+
+            const randomRepo = json.items[randomIndex];
+            return {
+                name: randomRepo.name,
+                language: randomRepo.language,
+                description: randomRepo.description ?? "No description provided.",
+                stars: randomRepo.stargazers_count,
+                forks: randomRepo.forks_count,
+                openIssues: randomRepo.open_issues_count,
+            };
+        } else {
+            return null;
+        }
+    }catch
+        (error)
+    {
+        console.error(error.message);
+        throw error;
+    }}
+
